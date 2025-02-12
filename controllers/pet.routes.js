f2ec94cc33ee164a9977e2d617d7599fca8a5bca
omarakamal <omaraakamal@gmail.com>
2025-02-12 19:50:28 +0300

    updated code

--- controllers/pet.routes.js
+++ controllers/pet.routes.js
@@ -7,8 +7,18 @@ const router = require("express").Router()
 // 1. endpoint path
 // 2. function that will run when we get the request
 
-router.get("/pets",(req,res)=>{
+router.get("/",(req,res)=>{
     res.json({message:"Success"})
+})
+
+router.post("/",async (req,res)=>{
+    try{
+        const createdPet = await Pet.create(req.body)
+        res.status(201).json(createdPet)    
+    }
+    catch(err){
+        res.status(500).json({err: err.message})
+    }
 })
 
 // export the router
--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(logger('dev'));
 
 // Routes go here
-app.use("/",petController)
+app.use("/pets",petController)
 
 app.listen(3000, () => {
   console.log('The express app is ready!');
